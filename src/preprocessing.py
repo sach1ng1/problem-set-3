48ff1fd0ab790fd441242fde92153dd0f6322590
sach1ng1 <saching1@umd.edu>
2026-04-13 22:30:03 -0400

    preprocessing the data for modeling

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -16,8 +16,9 @@ def load_data():
         model_pred_df (pd.DataFrame): DataFrame containing model predictions
         genres_df (pd.DataFrame): DataFrame containing genre information
     '''
-    # Your code here
-
+    genres_df=pd.read_csv("data/genres.csv")
+    model_pred_df = pd.read_csv("data/prediction_model_03.csv")
+    return genres_df, model_pred_df
 
 def process_data(model_pred_df, genres_df):
     '''
@@ -30,4 +31,32 @@ def process_data(model_pred_df, genres_df):
         genre_fp_counts (dict): Dictionary of false positive genre counts
     '''
 
-    # Your code here
+    genre_list=list(genres_df["genre"])
+    
+    genre_true_counts={}
+    genre_tp_counts = {}
+    genre_fp_counts={}
+    
+    for genre in genre_list:
+        genre_true_counts[genre]=0
+        genre_tp_counts[genre]=0
+        genre_fp_counts[genre]=0
+    
+    for movie_index in range(len(model_pred_df)):
+        movie_genre= model_pred_df.iloc[movie_index]
+        predicted_genre= movie_genre["predicted"]
+        correct_movie_genre= movie_genre["correct?"]
+
+        #actual genres column in the prediction model df holds a string that gives to a list of genres of a movie so to compare I will ned to cleaan it
+        clean_actual_genre= movie_genre["actual genres"].replace("'", ""). replace("]","").replace("[", "").split(",")
+        
+        for genre in clean_actual_genre:
+            if genre in genre_true_counts:
+                genre_true_counts[genre]+=1
+        if predicted_genre in genre_list:
+            if correct_movie_genre == 1:
+                genre_tp_counts[predicted_genre]+=1
+            else:
+                genre_fp_counts[predicted_genre]+=1
+    return genre_list, genre_true_counts, genre_tp_counts, genre_fp_counts
+        
